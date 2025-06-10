beab186f376be92d9881555e15f912ffc367ddc8
vinycius <viny.antunes@gmail.com>
2025-06-10 16:50:44 -0300

    resolvendo probelma de localdate no usuarioDAO.  CRUD COMPLETO!

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -6,11 +6,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+
 import model.Usuario;
-import util.ConexaoDB;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import util.ConexaoDB;
 
 public class UsuarioDAO {
